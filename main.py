e12107890592f0e09e1825fe7b6c8964928f4945
pahreeyy <fachrysyarifzu@gmail.com>
2024-12-08 16:19:07 +0700

    update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ pm25 = st.number_input("PM25", min_value=0, step=1, value=25)
 pm10 = st.selectbox("PM10", options=[0, 1], format_func=lambda x: "Female" if x == 0 else "Male")
 no2 = st.selectbox("NO2", options=[0, 1, 2], format_func=lambda x: ["Low", "Normal", "High"][x])
 so2 = st.selectbox("SO2", options=[0, 1], format_func=lambda x: "Normal" if x == 0 else "High")
-co = st.number_input("CO", min_value=0.00000, format="%.2f")
+co = st.number_input("CO", min_value=0.0, format="%.6f")
 
 if st.button("Prediksi"):
     # Data input berbentuk array 2D
