f812c379650c310fae5cf2d11404d096511707f3
pahreeyy <fachrysyarifzu@gmail.com>
2024-12-08 17:01:15 +0700

    update UI

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ st.markdown(
 # Input dari pengguna
 temperature = st.slider("Temperature (°C)", min_value=0.0, max_value=50.0, value=25.0, step=0.1)
 humidity = st.slider("Humidity (%)", min_value=0.0, max_value=100.0, value=50.0, step=0.1)
-pm25 = st.slider("PM25", min_value=0.0, max_value=500.0, value=30.0, step=0.1)
+pm25 = st.slider("PM.25", min_value=0.0, max_value=500.0, value=30.0, step=0.1)
 pm10 = st.slider("PM10", min_value=0.0, max_value=500.0, value=50.0, step=0.1)
 no2 = st.slider("NO2", min_value=0.0, max_value=100.0, value=20.0, step=0.1)
 so2 = st.slider("SO2", min_value=0.0, max_value=100.0, value=10.0, step=0.1)
