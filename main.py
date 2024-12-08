f3612e11cf58bdda2e9fa3df6807ea7d0018708b
pahreeyy <fachrysyarifzu@gmail.com>
2024-12-08 16:39:09 +0700

    update

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ pm25 = st.number_input("PM25", min_value=0.0, format="%.2f")
 pm10 = st.number_input("PM10", min_value=00.0, format="%.2f")
 no2 = st.number_input("NO2", min_value=00.0, format="%.2f")
 so2 = st.number_input("SO2", min_value=0.0, format="%.2f")
-co = st.number_input("CO", min_value=0, format="%.2f")
+co = st.number_input("CO", min_value=0.0, format="%.2f")
 proximity_to_industrial_areas = st.number_input("Proximity To Industrial Areas", min_value=0.0, format="%.2f")
 population_density = st.number_input("Population Density", min_value=0)
 
